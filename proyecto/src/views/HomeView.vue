73e307359f3fbf36d9f340c07c9d213cfd108192
Daniel Parra <danielparraalvarez@gmail.com>
2022-11-18 01:14:39 -0300

    Avance 1

---
+++ proyecto/src/views/HomeView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="bg-top">
+    <h3>Nombre</h3>
+    <h3>Promedio</h3>
+  </div>
+  <div>
+    <div class="container mb-5">
+      <div class="row size-row">
+        <div class="col-lg-4 mb-4">
+          <div class="card">
+            <img src="../assets/math.jpg" class="card-img-top" />
+            <div class="card-body">
+              <h5 class="card-title">Calculo I</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 mb-4">
+          <div class="card">
+            <img src="../assets/math.jpg" class="card-img-top" />
+            <div class="card-body">
+              <h5 class="card-title">Fisica 2</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 mb-4">
+          <div class="card">
+            <img src="../assets/add.png" class="card-img-top" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style>
+.hover {
+  cursor: pointer;
+}
+
+.bg-top {
+  background-color: rgb(127, 208, 255);
+}
+
+.size-row {
+  height: 1px;
+}
+</style>
